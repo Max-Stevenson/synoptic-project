fbf5d8fdddfcd203ced5903eaae88dcbde3fff04
Max Stevenson <maxstevenson@msn.com>
2019-11-25 14:05:18 +0000

    Manual logout route implemented.

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const User = require('../models/user');
+const auth = require ('../middleware/authentication');
 const router = new express.Router();
 
 router.post('/users', async (req, res) => {
@@ -28,6 +29,18 @@ router.post('/users/login', async (req, res) => {
     });
   } catch (error) {
     res.status(400).send();
+  };
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+      req.user.tokens = req.user.tokens.filter((token) => {
+          return token.token !== req.token;
+      });
+      await req.user.save();
+      res.status(200).send();
+  } catch (error) {
+      res.status(500).send();
   };
 });
 
